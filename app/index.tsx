a6fedec52fec41b822a0a3c9fdda4225fcf599bf
Jérémy Barbet <jeremgraph@gmail.com>
2025-01-31 22:09:14 +0000

    feat: add repro for rt theme

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,38 @@
-import { Text, View } from 'react-native';
-import { StyleSheet } from 'react-native-unistyles';
+import { Button, View } from 'react-native';
+import { StyleSheet, UnistylesRuntime, useUnistyles } from 'react-native-unistyles';
 
-const Index = () => (
-  <View style={s.wrapper}>
-    <Text>Edit app/index.tsx to edit this screen.</Text>
-  </View>
-);
+const Index = () => {
+  const { rt } = useUnistyles();
 
-const s = StyleSheet.create({
+  // OK
+  // const themeName = rt.themeName;
+
+  // OK
+  // const themeName = rt.themeName === 'dark' ? 'light' : 'dark';
+
+  return (
+    <View style={s.wrapper}>
+      <Button
+        title="Switch theme"
+        // OK
+        // onPress={() => UnistylesRuntime.setTheme(themeName === 'dark' ? 'light' : 'dark')}
+        // OK
+        // onPress={() => UnistylesRuntime.setTheme(themeName)}
+        // NOT OK
+        onPress={() => UnistylesRuntime.setTheme(rt.themeName === 'dark' ? 'light' : 'dark')}
+      />
+    </View>
+  );
+};
+
+const s = StyleSheet.create(theme => ({
   wrapper: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: theme.background,
   },
-});
+}));
 
 // eslint-disable-next-line import/no-unused-modules
 export default Index;
