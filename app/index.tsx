ff38bd61d551cc5777ae2f3dc4c96ed21873c969
Jérémy Barbet <jeremgraph@gmail.com>
2025-05-13 15:21:01 +0200

    feat: add repro

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,38 @@
-import { Text, View } from 'react-native';
+import { Pressable, Text, View } from 'react-native';
 import { StyleSheet } from 'react-native-unistyles';
 
 const Index = () => (
   <View style={s.wrapper}>
-    <Text>Edit app/index.tsx to edit this screen.</Text>
+    <Pressable style={s.button}>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <View style={s.dropShadow} />
+    </Pressable>
   </View>
 );
 
-const s = StyleSheet.create({
+const s = StyleSheet.create(theme => ({
   wrapper: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
-});
+
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    padding: 20,
+  },
+
+  dropShadow: {
+    position: 'absolute',
+    inset: 0,
+    zIndex: -2,
+
+    backgroundColor: theme.primary,
+    boxShadow: '0 4px 12px rgba(0, 0, 0, 0.15)',
+  },
+}));
 
 // eslint-disable-next-line import/no-unused-modules
 export default Index;
